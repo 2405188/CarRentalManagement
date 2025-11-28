572d2bbe39d1d3347a3e7ba876fe4ea6689b51d5
KUAH LING KANG COLIN <2405188J@student.tp.edu.sg>
2025-11-28 15:47:03 +0800

    Added Identity

--- CarRentalManagement/Data/CarRentalManagementContext.cs
+++ CarRentalManagement/Data/CarRentalManagementContext.cs
@@ -1,27 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using CarRentalManagement.Configurations.Entities;
+using CarRentalManagement.Data;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using CarRentalManagement.Domain;
-using CarRentalManagement.Configurations.Entities;
 
 namespace CarRentalManagement.Data
 {
-    public class CarRentalManagementContext : DbContext
+    public class CarRentalManagementContext(DbContextOptions<CarRentalManagementContext> options) : IdentityDbContext<CarRentalManagementUser>(options)
     {
-        public CarRentalManagementContext (DbContextOptions<CarRentalManagementContext> options)
-            : base(options)
-        {
-        }
-
         public DbSet<CarRentalManagement.Domain.Make> Make { get; set; } = default!;
         public DbSet<CarRentalManagement.Domain.Model> Models { get; set; } = default!;
         public DbSet<CarRentalManagement.Domain.Colour> Colour { get; set; } = default!;
         public DbSet<CarRentalManagement.Domain.Vehicle> Vehicle { get; set; } = default!;
         public DbSet<CarRentalManagement.Domain.Booking> Booking { get; set; } = default!;
         public DbSet<CarRentalManagement.Domain.Customer> Customer { get; set; } = default!;
-
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -33,3 +24,4 @@ namespace CarRentalManagement.Data
         }
     }
 }
+
---
+++ CarRentalManagement/Data/CarRentalManagementUser.cs
@@ -0,0 +1,12 @@
+﻿using CarRentalManagement.Configurations.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRentalManagement.Data
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class CarRentalManagementUser : IdentityUser
+    {
+
+    }
+}
